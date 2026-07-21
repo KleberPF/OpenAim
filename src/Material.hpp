5e895200c559125dc12106ec7f8a35e90cb5a164
Kleber <kleber.pandino@gmail.com>
2026-07-21 17:55:04 -0300

    Fix missing <functional> import

--- src/Material.hpp
+++ src/Material.hpp
@@ -6,6 +6,7 @@
 
 #include <array>
 #include <vector>
+#include <functional>
 
 class Texture {
 public:
--- src/Sprite.hpp
+++ src/Sprite.hpp
@@ -4,6 +4,7 @@
 #include "Shader.hpp"
 
 #include <glm/glm.hpp>
+#include <functional>
 
 // Kinda ugly
 struct SpritePosition {
